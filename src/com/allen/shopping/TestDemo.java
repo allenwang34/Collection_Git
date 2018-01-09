e9160a9d4a00d1f82f2302a3b31dbb28d22ab58b
Yiyang Wang Mac <allenwang950321@gmail.com>
2018-01-09 17:47:32 +0800

    Goods Management functionality achieved

--- src/com/allen/shopping/TestDemo.java
+++ src/com/allen/shopping/TestDemo.java
@@ -1,21 +1,70 @@
 package com.allen.shopping;
+import java.util.Scanner;
 
 public class TestDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		boolean keepWorking = true;
+		Scanner sc = new Scanner(System.in);
 		GoodsManage GM = new GoodsManage();
-		GM.importGoods();
-		GM.displayAllGoods();
-		GM.addItems(new Goods("G5","MacBook Air", 12888,"This is a MacBook"));
-		System.out.println("*********************************");
-		GM.displayAllGoods();
-		System.out.println("*********************************");
+		do {
+			displayMainMenu();
+			System.out.println("************************************************************************");
+			System.out.println("Please give me an input: ");
+			int userInput = sc.nextInt();
+			switch(userInput) {
+				case 1 : 
+					boolean keepGMMenu = true;
+					do {
+						displayGMMenu();
+						System.out.println("************************************************************************");
+						System.out.println("Please give me an input: ");
+						int GMInput = sc.nextInt();
+						switch (GMInput) {
+							case 9: 
+								keepGMMenu=false;
+								break;
+							case 1:
+								GM.importGoods();
+								System.out.println("************************************************************************");
+								System.out.println("Goods input finished");
+								break;
+							case 2: 
+								System.out.println("************************************************************************");
+								GM.displayAllGoods();
+						}
+					} while (keepGMMenu);
+					break;
+				case 0:
+					keepWorking=false;
+					break;
+					
+			
+			}
+		}while(keepWorking);
+		System.out.println("************************************************************************");
+		System.out.println("                            **END**                                     ");
 		
-		
-
 	}
 	
+	public static void displayMainMenu() {
+		System.out.println("************************************************************************");
+		System.out.println("                            **Main Menu**                               ");
+		System.out.println("                       1--Goods Management                              ");
+		System.out.println("                       2--Shopping Cart                                 ");
+		System.out.println("                       0--Exit                                          ");
+		
+	}
+	
+	public static void displayGMMenu() {
+		System.out.println("************************************************************************");
+		System.out.println("                            **Goods Management**                        ");
+		System.out.println("                       1--Goods Info Input                              ");
+		System.out.println("                       2--Display All Goods Info                        ");
+		System.out.println("                       9--Back To Previous Menu                         ");
+		
+	}
 	
 
 }
